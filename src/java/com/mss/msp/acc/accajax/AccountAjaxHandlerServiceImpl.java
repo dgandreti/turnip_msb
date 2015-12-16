a1cbb48e2312998f80ce79adc56349e2db1aefba
vsiram <vsiram@miraclesoft.com>
2015-12-16 13:20:25 -0500

    Updated :12/16/2015-ReqAddJsp

--- src/java/com/mss/msp/acc/accajax/AccountAjaxHandlerServiceImpl.java
+++ src/java/com/mss/msp/acc/accajax/AccountAjaxHandlerServiceImpl.java
@@ -1025,6 +1025,7 @@ public class AccountAjaxHandlerServiceImpl implements AccountAjaxHandlerService 
             resultSet = statement.executeQuery(queryString);
 
             while (resultSet.next()) {
+                System.out.println("Commnets By Prasad");
                 resultString += resultSet.getString("csr_id") + "|" + resultSet.getString("NAME") + "|" + resultSet.getString("STATUS") + "|" + resultSet.getString("email1") + "|" + resultSet.getString("phone1") + "|" + com.mss.msp.util.DataSourceDataProvider.getInstance().getFnameandLnamebyStringId(resultSet.getString("created_by")) + "|" + com.mss.msp.util.DateUtility.getInstance().convertToviewFormatInDash(resultSet.getString("created_date")) + "^";
             }
 
